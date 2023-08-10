f511a0a86766bb390bfaf8e3620108459c05f68f
Nilton Caio <ciaonilton@hotmail.com>
2023-08-10 14:09:28 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault()
+
+        const inputTask = $('#input_task').val()
+        const newList = $('<li></li>')
+        $(`<p>${inputTask}</p>`).appendTo(newList)
+        $(newList).appendTo('ul')
+        $(newList).fadeIn(500)
+        $('#input_task').val('')
+    })
+
+    $('ul').on('click','li', function(){
+        $('p').css('text-decoration', 'line-through');
+    })
+
+    $('#reset_ul').click('reset', function() {
+        $('ul').empty()
+    })
+})
